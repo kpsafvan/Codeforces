b6ddd3fc7d508470fb03fab6fd51573f377b233f
kpsafvan <safvankpmes@gmail.com>
2023-09-24 22:42:31 +0530

    update skeleton

--- contest 55/skelp - Copy (4) - Copy - Copy.py
+++
@@ -1,8 +0,0 @@
-import sys
-from sys import stdin, stdout 
-def get_ints(): return list(map(float, sys.stdin.readline().strip().split()))
-def get_string(): return sys.stdin.readline().strip() 
-n =int(input())
-for i in range(0,n):
-	a = get_string()
-	print(len(a))
