34979eed7f505cc670a4b465dc73d94911f2a833
kpsafvan <safvankpmes@gmail.com>
2025-04-21 19:54:45 +0530

    contest 67

---
+++ contest 67/a.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<math.h>
+#include<algorithm>
+#include<string.h>
+
+using namespace std; 
+int main(){
+	
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long long i,n,t,j,m;
+   	cin>>t;
+   	for(i=0;i<t;i++)
+   	{
+   		int k;
+   		 cin>>k;
+   		
+   	}
+}
